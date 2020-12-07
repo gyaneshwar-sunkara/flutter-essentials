2a2e85ae962d5926c2ae20658fd95d7951666667
Gyani Sunkara <gyanisunkara1@gmail.com>
2020-12-07 23:12:39 +0530

    Theme update - font poppins

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: _title,
+      theme: ThemeData(fontFamily: 'Poppins'),
       home: MyStatefulWidget(),
     );
   }
